e0d0b8e11e9e9b71c405d7a5ffe6594f4071d40a
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-05-05 12:13:52 +0300

    Update JSONBundle.swift

--- JSONBundle.swift
+++ JSONBundle.swift
@@ -3,12 +3,12 @@ extension JSONBundle {
     func decode(_ file: String) -> [String: Astronaut] {
         // "url" adlı bir değişkene, "file" parametresinin adını belirterek uygulamanın ana bundle'ındaki JSON dosyasının URL'sini atıyoruz.
         guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
+            fatalError("Bundle içinde \(file) bulunamadı.")
         }
 
         // "data" adlı bir değişkene, "url" değişkenindeki URL'den JSON dosyasının içeriğini yüklüyoruz.
         guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
+            fatalError("Bundle'dan \(file) yüklenemedi.")
         }
 
         // "decoder" adlı bir değişkene JSONDecoder sınıfını kullanarak bir JSON çözücü nesnesi oluşturuyoruz.
@@ -16,10 +16,14 @@ extension JSONBundle {
 
         // "loaded" adlı bir değişkene "data" değişkenindeki JSON verilerini çözümleyerek [String: Astronaut] türündeki bir sözlüğe atıyoruz.
         guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
-            fatalError("Failed to decode \(file) from bundle.")
+            fatalError("JSONDecoder ile \(file) çözümlenemedi.")
         }
 
         // "loaded" sözlüğünü geri döndürüyoruz.
         return loaded
     }
 }
+
+
+// Kullanımı
+let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
